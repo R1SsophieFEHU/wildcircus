c2cac1eac7445a0edf301c36ae139b337c07bc4b
Sophie <strochain@gmail.com>
2020-01-29 16:56:45 +0100

    création base de donnée

--- database/wildcircus.sql
+++ database/wildcircus.sql
@@ -2,26 +2,30 @@ CREATE DATABASE wildcircus;
 
 USE wildcircus;
 
-CREATE TABLE show
+DROP TABLE if exists spectacles;
+DROP TABLE if exists artiste;
+DROP TABLE if exists newsletter;
+
+CREATE TABLE spectacles
 (
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title VARCHAR(100),
-  category VARCHAR (50)
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+title VARCHAR(100),
+category VARCHAR (50)
 );
 
-CREATE TABLE artist
+CREATE TABLE artiste
 (
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  lastname VARCHAR(30),
-  firstname VARCHAR(30),
-  category VARCHAR(50),
-  arrival_date DATE
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+lastname VARCHAR(30),
+firstname VARCHAR(30),
+category VARCHAR(50),
+arrival_date DATE
 );
 
 CREATE TABLE newsletter
 (
-  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-  email VARCHAR (32O) NOT NULL,
-  lastname VARCHAR(30),
-  firstname VARCHAR(30) 
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+email VARCHAR (320) NOT NULL,
+lastname VARCHAR(30),
+firstname VARCHAR(30) 
 );
