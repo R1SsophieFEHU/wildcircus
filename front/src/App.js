668c880047a40043745f07f66468ddbf6cd71181
Sophie <strochain@gmail.com>
2020-01-30 12:03:01 +0100

    ajout styles

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/common.css";
+import "./styles/space.css";
+import "./styles/layout.css";
 import NewsLetter from './components/NewsLetter'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
       </header>
       <NewsLetter/>
     </div>
--- front/src/components/NewsLetter.jsx
+++ front/src/components/NewsLetter.jsx
@@ -3,19 +3,23 @@ import React from 'react';
 
 function NewsLetter() {
   return (
-    <div>
+    <div className="flex:row flex-both:center">
+      <div>
       <h4> Want to follow our news ?</h4>
         <p> Subscribe to the newsletter to receive in your inbox all the news of the site!</p>
-        <form>
-          <label htmlFor="Lastname">Lastname</label>
-          <input type="text" id="Lastname" name="Lastname" required></input>
-          <label htmlFor="Firstname">Firstname</label>
-          <input type="text" id="Firstname" name="Firstname" required></input>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" required></input>
+        </div>
+        <form className="flex:column flex-both:center">
+        <div className="flex:column flex-cross:center space:stack">
+          <label className="space:stack" htmlFor="Lastname">Lastname</label>
+          <input className="flex:1 space:inset-squish" type="text" id="Lastname" name="Lastname" required></input>
+          <label className="space:stack" htmlFor="Firstname">Firstname</label>
+          <input className="flex:1 space:inset-squish" type="text" id="Firstname" name="Firstname" required></input>
+          <label className="space:stack" htmlFor="email">Email</label>
+          <input className="flex:1 space:inset-squish" type="email" id="email" name="email" required></input>
           <button>Send
 
           </button>
+          </div>
         </form>
 
     </div>
