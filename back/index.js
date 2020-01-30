712291330a46125ebee367a97e00079f522aec6c
Sophie <strochain@gmail.com>
2020-01-30 11:04:02 +0100

    ajout routes GET et POST + tests

--- back/index.js
+++ back/index.js
@@ -16,8 +16,48 @@ app.use(
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Express");
+});
 
+app.get("/artists", (req, res)=> {
+  database.query(
+    "SELECT * FROM artist", (err, results)=> {
+      console.log(results);
+      if (err){
+        res.status(500).send("Error get artists");
+      } else {
+        res.json(results);
+      }
+    }
+  )
+})
 
+app.get("/representations", (req, res)=> {
+  database.query(
+    "SELECT * FROM representation", (err, results)=> {
+      console.log(results);
+      if (err){
+        res.status(500).send("Error get representations")
+      } else {
+        res.json(results);
+      }
+    }
+  )
+})
+
+app.post("/newsletter", (req, res) => {
+  const { email, lastname, firstname } = req.body;
+  const formAdd = { email, lastname, firstname };
+  database.query("INSERT INTO newsletter SET ?", formAdd, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error saving email info");
+    } else {
+      res.status(201).send({ ...formAdd, id: result.insertId });
+    }
+  });
+});
 
 
 
