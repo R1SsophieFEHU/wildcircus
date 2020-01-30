da35ddac6700529ec52189d1d509026978ebea3f
Sophie <strochain@gmail.com>
2020-01-30 09:26:15 +0100

    ajout info dans BDD'

--- database/wildcircus.sql
+++ database/wildcircus.sql
@@ -2,24 +2,26 @@ CREATE DATABASE wildcircus;
 
 USE wildcircus;
 
-DROP TABLE if exists spectacles;
-DROP TABLE if exists artiste;
+DROP TABLE if exists representation;
+DROP TABLE if exists artist;
 DROP TABLE if exists newsletter;
+DROP TABLE if exists representation_artist;
 
-CREATE TABLE spectacles
+CREATE TABLE representation
 (
 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 title VARCHAR(100),
+description VARCHAR (350),
 category VARCHAR (50)
 );
 
-CREATE TABLE artiste
+CREATE TABLE artist
 (
 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 lastname VARCHAR(30),
 firstname VARCHAR(30),
-category VARCHAR(50),
-arrival_date DATE
+description VARCHAR (250),
+avatar varchar(255) DEFAULT NULL
 );
 
 CREATE TABLE newsletter
@@ -28,4 +30,10 @@ id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 email VARCHAR (320) NOT NULL,
 lastname VARCHAR(30),
 firstname VARCHAR(30) 
+);
+
+CREATE TABLE representation_artist
+(
+  artist_id INT NOT NULL,
+  representation_id INT NOT NULL
 );
