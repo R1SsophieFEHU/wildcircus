dd16a4bf8186d4fcb63a8e0131702ed62126090d
Sophie <strochain@gmail.com>
2020-01-30 16:10:37 +0100

    header

--- front/src/App.js
+++ front/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import "./styles/common.css";
 import "./styles/space.css";
 import "./styles/layout.css";
-import NewsLetter from './components/NewsLetter'
+import NewsLetter from './components/NewsLetter';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     
-      </header>
+      <Header />
+
       <NewsLetter/>
     </div>
     
---
+++ front/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <div className="header">
+      <p>Wild Circus</p>
+    </div>
+    
+  );
+}
+
+export default Header;
