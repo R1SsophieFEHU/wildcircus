76f8fffad48c2b80bbec7f90c282fc1463b1ce68
Sophie <strochain@gmail.com>
2020-01-30 14:07:06 +0100

    composant newsletter fonctionnel

--- front/src/components/NewsLetter.jsx
+++ front/src/components/NewsLetter.jsx
@@ -1,29 +1,87 @@
 import React from 'react';
+import axios from 'axios';
 
 
-function NewsLetter() {
-  return (
-    <div className="flex:row flex-both:center">
-      <div>
-      <h4> Want to follow our news ?</h4>
-        <p> Subscribe to the newsletter to receive in your inbox all the news of the site!</p>
-        </div>
-        <form className="flex:column flex-both:center">
-        <div className="flex:column flex-cross:center space:stack">
-          <label className="space:stack" htmlFor="Lastname">Lastname</label>
-          <input className="flex:1 space:inset-squish" type="text" id="Lastname" name="Lastname" required></input>
-          <label className="space:stack" htmlFor="Firstname">Firstname</label>
-          <input className="flex:1 space:inset-squish" type="text" id="Firstname" name="Firstname" required></input>
-          <label className="space:stack" htmlFor="email">Email</label>
-          <input className="flex:1 space:inset-squish" type="email" id="email" name="email" required></input>
-          <button>Send
+class NewsLetter extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      newsletterInfos: [],
+      email : "",
+      lastname : "",
+      firstname : ""
+    };
+  }
 
-          </button>
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  sendEmail = () => {
+    axios
+      .post("http://localhost:8000/newsletter", {
+
+        lastname : this.state.lastname,
+        firstname : this.state.firstname,
+        email: this.state.email
+      })
+      .then(res => {
+        this.setState({newsletterInfos : [...this.state.newsletterInfos, res.data] }, () => alert("Email will be send to you"));
+      });
+  }; 
+  
+
+  render (){
+    return (
+      <div className="flex:row flex-both:center ">
+        <div style={{marginRight:"7vh"}}>
+        <h4> Want to follow our news ?</h4>
+          <p> Subscribe to the newsletter to receive in your inbox all the news of the site!</p>
           </div>
-        </form>
+          <form className="flex:column">
+            <div className="flex:column flex-cross:center space:stack ">
+              <label className="space:stack" htmlFor="lastname">Lastname</label>
+              <input 
+              className="flex:1 space:inset-squish" 
+              type="text" 
+              id="lastname" 
+              name="lastname" 
+              onChange={this.handleChange}
+              value={this.state.lastname}
+              required>
 
-    </div>
-  );
+              </input>
+              <label className="space:stack" htmlFor="firstname">Firstname</label>
+              <input 
+              className="flex:1 space:inset-squish" 
+              type="text" 
+              id="firstname" 
+              name="firstname" 
+              onChange={this.handleChange}
+              value={this.state.firstname}
+              required>
+              </input>
+
+              <label className="space:stack" htmlFor="email">Email</label>
+              <input 
+              className="flex:1 space:inset-squish" 
+              type="email" 
+              id="email" 
+              name="email" 
+              onChange={this.handleChange}
+              value={this.state.email}
+              required>                
+              </input>
+
+              <button type="submit" onClick = {this.sendEmail}>Send</button>
+            </div>
+          </form>
+
+      </div>
+    );
+  }
 }
 
 export default NewsLetter;
